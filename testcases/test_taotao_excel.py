08bc56952e44a1a390e3dbd06476e0c66f40ba37
snowlts <snowlts@163.com>
2024-05-06 20:54:28 +0800

    support allure

--- testcases/test_taotao_excel.py
+++ testcases/test_taotao_excel.py
@@ -1,3 +1,4 @@
+import allure
 import requests
 import json
 import pytest
@@ -10,45 +11,48 @@ from utils.AssertUtil import Assert
 from utils.DbUtil import db
 from utils.LogUtil import my_log
 
-from common.common import parameterize,json_load,get_sql_field
+from common.common import parameterize, json_load, get_sql_field,get_allure_severity
+
 #
 base_url = config.url or 'http://127.0.0.1:8000/'
 logger = logger_init()
 my_assert = Assert()
+
+
 class Test_Taotao(object):
     def setup_class(self):
         self.base_url = base_url
         self.request = Request()
-        logger.debug("base_url: %s" %  self.base_url)
+        logger.debug("base_url: %s" % self.base_url)
 
-    def _run_case(self,testcase):
+    def _run_case(self, testcase):
         logger.info("用例编号:%s,接口:%s,测试点:%s" % (testcase['用例编号'], testcase['接口'], testcase['测试点']))
         data, headers, cookies = testcase['请求参数'], testcase['headers'], testcase['cookies']
 
-        #运行前置用例，并获取返回值
+        # 运行前置用例，并获取返回值
         if testcase['前置条件']:
-            dep_data =self._run_dependent_case(testcase['前置条件'])
-            #参数替换
-            data,headers,cookies = parameterize.bat_sub([data,headers,cookies],dep_data)
-        data,headers,cookies = json_load(data,testcase['参数类型']),json_load(headers),json_load(cookies)
+            dep_data = self._run_dependent_case(testcase['前置条件'])
+            # 参数替换
+            data, headers, cookies = parameterize.bat_sub([data, headers, cookies], dep_data)
+        data, headers, cookies = json_load(data, testcase['参数类型']), json_load(headers), json_load(cookies)
 
         ##发送http请求
         url = self.base_url + testcase['path']
-        method= testcase['请求方法']
+        method = testcase['请求方法']
         logger.info("send %s request: url: %s, data: %s" % (method, url, data))
-        r = self.request.send(url, method, data=data,headers=headers,cookies=cookies)
+        r = self.request.send(url, method, data=data, headers=headers, cookies=cookies)
 
-        #获取返回值，并return
+        # 获取返回值，并return
         logger.debug("http response: %s" % r)
         return r
 
-    def _run_dependent_case(self,case_id):
-        #根据case_id获取用例
+    def _run_dependent_case(self, case_id):
+        # 根据case_id获取用例
         testcase = testcases.data[case_id]
         r = self._run_case(testcase)
         return r['data']
 
-    def _assert_case(self,http_response,testcase):
+    def _assert_case(self, http_response, testcase):
         code, body = http_response['code'], http_response['data']
         exp_code, exp_body, exp_contains, exp_fields, exp_sql = (int(testcase['期望返回值code']),
                                                                  testcase['期望返回值body'],
@@ -69,7 +73,7 @@ class Test_Taotao(object):
         if exp_fields:
             exp_fields = json.loads(exp_fields.encode('utf-8'))
             fields = {k: v for k, v in body.items() if k in exp_fields.keys()}
-            print(exp_fields,fields)
+            print(exp_fields, fields)
             print(body.items())
             my_assert.assert_body_fields(fields, exp_fields)
         if exp_sql:
@@ -78,8 +82,26 @@ class Test_Taotao(object):
             db_data = db.fetchall(testcase['通过数据库验证'])[0]
             print('db:', db_data)
             my_assert.assert_body_fields(fields, db_data)
-    @pytest.mark.parametrize('testcase',testcases.data.values())
-    def test_run(self,testcase):
-        http_response =self._run_case(testcase)
-        self._assert_case(http_response,testcase)
 
+    @pytest.mark.parametrize('testcase', testcases.data.values())
+    def test_run(self, testcase):
+        allure.dynamic.feature(testcase['接口'])
+        allure.dynamic.title("{}:{}".format(testcase['用例编号'], testcase['测试点']))
+        allure.dynamic.description(
+            "<b>path</b>:{}<br/>;\
+            <b>method</b>:{}<br/>;\
+            <b>期望返回值code</b>:{}<br/>;\
+            <b>期望返回值body</b>:{}<br/>;\
+            <b>期望返回值contains</b>:{}<br/>;\
+            <b>期望返回值字段值</b>:{}<br/>;\
+            <b>通过数据库验证</b>:{}<br/>".format(
+                testcase['接口'],
+                testcase['请求方法'],
+                testcase['期望返回值code'],
+                testcase['期望返回值body'],
+                testcase['期望返回值contains'],
+                testcase['期望返回值字段值'],
+                testcase['通过数据库验证']))
+        allure.dynamic.severity(get_allure_severity(testcase['严重等级']))
+        http_response = self._run_case(testcase)
+        self._assert_case(http_response, testcase)
